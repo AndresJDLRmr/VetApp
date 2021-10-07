5b53268c10bafa41534cacae198e7111ff0b8a4f
Ramirors31 <ramirezsiqueiros@hotmail.com>
2021-10-07 14:12:17 -0500

    Actualizacion a veterinaria

--- app/src/main/java/com/example/westcutsbarbershop/data/BarbersDatasource.kt
+++ app/src/main/java/com/example/westcutsbarbershop/data/BarbersDatasource.kt
@@ -5,10 +5,9 @@ import com.example.westcutsbarbershop.R
 class BarbersDatasource {
     fun loadItems():List<Barbers>{
         return listOf<Barbers>(
-            Barbers(R.string.name_barber1,R.string.days_barber1,R.string.hours_barber,R.drawable.juanignacio),
-            Barbers(R.string.name_barber2,R.string.days_barber2,R.string.hours_barber,R.drawable.elvato),
-            Barbers(R.string.name_barber3,R.string.days_barber3,R.string.hours_barber,R.drawable.rodney),
-            Barbers(R.string.name_barber4,R.string.days_barber4,R.string.hours_barber,R.drawable.elbryan)
+            Barbers(R.string.pet1,R.string.type_pet1,R.string.age_pet1,R.drawable.dogo),
+            Barbers(R.string.pet2,R.string.type_pet2,R.string.age_pet2,R.drawable.gato),
+            Barbers(R.string.pet3,R.string.type_pet3,R.string.age_pet3,R.drawable.loro),
 
         )
     }
